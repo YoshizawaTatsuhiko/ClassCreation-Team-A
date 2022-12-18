15b64b03c948064c51eb613498a98d3884ce85ad
mizuki_0113 <vanmizuki39@gmail.com>
2022-12-18 21:50:24 +0900

    PlayerControllerのキャラクターを動かすスクリプトの追加

--- Assets/Mizuki/Script/PlayerController.cs
+++ Assets/Mizuki/Script/PlayerController.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float _moveSpeed = 3;
+    Rigidbody _rb = default;
+
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Vector3 dir = Vector3.forward * v + Vector3.right * h;
+        // カメラのローカル座標系を基準に dir を変換する
+        dir = Camera.main.transform.TransformDirection(dir);
+        // カメラは斜め下に向いているので、Y 軸の値を 0 にして「XZ 平面上のベクトル」にする
+        dir.y = 0;
+        // 移動の入力がない時は回転させない。入力がある時はその方向にキャラクターを向ける。
+        if (dir != Vector3.zero) this.transform.forward = dir;
+        //_rb.velocity = dir.normalized * _moveSpeed;
+        //垂直方向の速度を保持する
+        Vector3 velocity = dir.normalized * _moveSpeed;
+        velocity.y = _rb.velocity.y;
+        _rb.velocity = velocity;
     }
 }
