9fb40a4f065db58f046853dbbcb7ab51938bbeed
tatsupiko <identity2022@outlook.jp>
2022-12-20 23:45:16 +0900

    EnemyControllerにコメントを記述。

--- Assets/Tatsuhiko/EnemyController.cs
+++ Assets/Tatsuhiko/EnemyController.cs
@@ -6,26 +6,32 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    /// <summary></summary>
+    /// <summary>Playerのゲームオブジェクト</summary>
     private GameObject _player = null;
-    [SerializeField, Tooltip("")]
+    [SerializeField, Tooltip("移動速度")]
     private float _speed = 1f;
-    /// <summary></summary>
+    /// <summary>EnemyのRigidbody</summary>
     private Rigidbody _rb = null;
 
     private void Start()
     {
+        //Playerを見つけておく
         _player = GameObject.FindGameObjectWithTag("Player");
+
+        //Enemy自身のRigidbodyを取得しておく
         _rb = GetComponent<Rigidbody>();
         _rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
     }
 
     private void Update()
     {
+        //Playerの方向に進む。
         Vector3 vec = _player.transform.position - transform.position;
         vec.y = 0;
         transform.forward = vec;
         _rb.velocity = transform.forward * _speed;
+
+        //Playerに一定距離近づいたら、止まる。
         Vector2 playerPos = new Vector2(_player.transform.position.x, _player.transform.position.z);
         Vector2 selfPos = new Vector2(transform.position.x, transform.position.z);
         if (Vector2.Distance(playerPos, selfPos) < 0.2f)
